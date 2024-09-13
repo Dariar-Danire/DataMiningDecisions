69f5b955d102599de3cb967695a195b511e29c89
daria <dar3018@yandex.ru>
2024-09-13 20:36:29 +0300

    Задача 8

--- __main__.py
+++ __main__.py
@@ -1,10 +1,14 @@
-from lab1 import task7
+from lab1 import task8
 import time
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    N = int(input("Введите количество чисел Фибоначчи: "))
-    print(list(task7.solve(N)))
+    #N = int(input("Введите количество чисел Фибоначчи: "))
+    #a = list(task7.solve(N))
+    #print(a)
+
+    N = int(input("Введите количество цифр: "))
+    print(task8.solve(N))
 
     print("\nEnd time: ", time.time() - start_time)
